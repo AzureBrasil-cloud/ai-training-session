1b71d3a00a064550fedec9f061e3bf7ae6629112
tallesdestefanidesouzavaliatti <talles.dsv@parsus.com>
2024-12-05 13:41:38 -0300

    Add new console app

---
+++ doc-assistant-app/DocAssistantApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder()
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile("appsettings.Development.json", optional: false, reloadOnChange: true);
+
+IConfiguration config = builder.Build();
+
+var model = config["AzureOpenAi:Model"]!;
+var key = config["AzureOpenAi:Key"]!;
+var endpoint = config["AzureOpenAi:Endpoint"]!;
+
+var a = 1;
